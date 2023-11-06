086456839f7891e52bff96b634f04902cefc5567
Evie Hoang <evie.h0325@gmail.com>
2023-11-06 13:50:31 -0800

    Added project gets queries for routes.

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -1,6 +1,30 @@
 const router = require('express').Router();
 const { Project } = require('../../models');
 
+router.get('/', async (req, res) => {
+  try {
+    const data = await Project.findAll({
+    });
+const projectData = data.map((project) => project.get({plain:true}));
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const data = await Project.findOne({
+    where: {
+      id: req.params.id,
+    },
+    });
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 router.post('/', async (req, res) => {
   try {
     const newProject = await Project.create({
